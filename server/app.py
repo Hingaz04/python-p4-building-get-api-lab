69dd94a9cd33136af07eef67d1c65a5bc99dd9fa
Hingaz <hhinga04@gmail.com>
2024-01-12 12:10:22 +0300

    code challenge

--- server/app.py
+++ server/app.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
@@ -14,25 +11,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = Bakery.query.all()
+    return jsonify(Bakery.serialize_list(all_bakeries))
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        return jsonify(bakery.serialize)
+    else:
+        return jsonify({"error": "Bakery not found"}), 404
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify(BakedGood.serialize_list(baked_goods))
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(
+        BakedGood.price.desc()).first()
+    if most_expensive_good:
+        return jsonify(most_expensive_good.serialize)
+    else:
+        return jsonify({"error": "No baked goods found"}), 404
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
